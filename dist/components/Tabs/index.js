19e261ea258725d08a99d0ab6e45a659830dc2c1
yeshaojun <sjye@rsvptech.cn>
2022-09-19 10:21:25 +0800

    fix: gitignore

--- dist/components/Tabs/index.js
+++
@@ -1,5 +0,0 @@
-import Tabs from "./tabs";
-import TabPane from "./tabPane";
-var TransTabs = Tabs;
-TransTabs.TabPane = TabPane;
-export default TransTabs;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as Alert } from "./components/Alert";
-export { default as Button } from "./components/Button";
-export { default as Icon } from "./components/Icon";
-export { default as Menu } from "./components/Menu";
-export { default as Tabs } from "./components/Tabs";
-export { default as Transition } from "./components/Transition";
